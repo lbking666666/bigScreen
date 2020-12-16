9fd4485bf250765882ece3322c1984d3ddd4eb3f
“lbking666666” <lbking666666@qq.com>
2020-12-16 15:51:53 +0800

    api

--- big-screen/src/main.js
+++ big-screen/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 引入mockjs
+require('./api/mock.js')
 createApp(App).use(router).mount('#app')
--- big-screen/src/views/Home.vue
+++ big-screen/src/views/Home.vue
@@ -1,21 +1,38 @@
 <template>
-  <div class="home">
-    <barChart></barChart>
-    <mapChart></mapChart>
-    <lineChart></lineChart>
-  </div>
+    <div class="home">
+        <barChart />
+        <mapChart />
+        <lineChart />
+    </div>
 </template>
-
 <script>
 // @ is an alias to /src
-import barChart from '@/components/barChart.vue'
-import mapChart from '@/components/mapChart.vue'
-import lineChart from '@/components/lineChart.vue'
-
+import barChart from '@/components/barChart.vue';
+import mapChart from '@/components/mapChart.vue';
+import lineChart from '@/components/lineChart.vue';
+import { addAreaUser } from '@/api/index.js';
 export default {
-  name: 'Home',
-  components: {
-    barChart,mapChart,lineChart
-  }
+    name: 'Home',
+    components: {
+        barChart,
+        mapChart,
+        lineChart
+    },
+    mounted() {
+        console.log(323)
+        this.getData()
+    },
+    methods: {
+        getData() {
+            console.log(222)
+            this.getAreaUser()
+        },
+        getAreaUser() {
+            console.log(1)
+            addAreaUser().then(res => {
+                console.log(res)
+            })
+        }
+    }
 }
 </script>
