07d37a396a56e31feb720d978e87403ae71f7e5b
“lbking666666” <lbking666666@qq.com>
2020-12-17 11:51:57 +0800

    eslint

--- big-screen/src/components/mapChart.vue
+++ big-screen/src/components/mapChart.vue
@@ -1,15 +1,23 @@
 <template>
     <div class="map-chart">
-        地图1
+        <div ref="map" style="width:100%;height:100%"></div>
     </div>
 </template>
-
 <script>
-    export default {
-        name: "mapChart"
+export default {
+    name: "mapChart",
+    data() {
+        return {
+            a: 1,
+            n: 1
+        }
+    },
+    method: {
+        show() {
+            console.log(1)
+        }
     }
+}
 </script>
-
 <style scoped>
-
 </style>
