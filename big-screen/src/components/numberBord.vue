2b0f3268f30163695e58b2bc69361ee8edb3e9a7
Silenter <2207252182@qq.com>
2020-12-24 14:58:19 +0800

    commit

--- big-screen/src/components/numberBord.vue
+++ big-screen/src/components/numberBord.vue
@@ -2,7 +2,22 @@
     <div class="number-bord">
         <div class="bord-title">{{title}}</div>
         <div class="number-box">
-            <div class="number-item" v-for="(item, index) in numberStr" :key="index"><div class="font" :class="item.comma ? 'comma' : ''">{{item.numStr}}</div></div>
+            <div class="number-item" v-for="(item, index) in numberStr" :key="index">
+                <div class="scroll-out-box" :class="item.comma ? 'comma' : ''">
+                    <div class="scroll-box" :class="'number-'+item.numStr">
+                        <div class="font">0</div>
+                        <div class="font">1</div>
+                        <div class="font">2</div>
+                        <div class="font">3</div>
+                        <div class="font">4</div>
+                        <div class="font">5</div>
+                        <div class="font">6</div>
+                        <div class="font">7</div>
+                        <div class="font">8</div>
+                        <div class="font">9</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -27,7 +42,7 @@
 			return{
                 numberStr: [],
                 timer: null,
-                fetchNum: 0,
+                fetchNum: 0, // 记录前一个数据
                 restTime: 0
 			}
         },
@@ -37,7 +52,7 @@
                     // console.log('watch mounted')
                     // ajax反应太慢，mounted执行可能与watch同步 
                     // 所以，设置该状态为初始化
-                    this.fetchNum = newVal
+                    this.fetchNum = oldVal
                     this.transNumber(newVal)
                 } else {
                     // console.log('do loop')
@@ -52,9 +67,9 @@
 		},
         methods:{
             transNumber( num ){
-                // console.log('transNumber work')
                 let nums = String(num).split('').reverse() // 获取之后反转顺序
                 let newArray = []
+                let oldArray = this.numberStr.reverse() // 用于逐字比较的字符串数组
                 for(let i=0; i<nums.length; i++){
                     let newItem = {
                         numStr: nums[i],
@@ -66,58 +81,15 @@
                     newArray.push(newItem)
                 }
                 this.numberStr = newArray.reverse()
-            },
-            /**
-             * 每5s从接口重新获取一次新的数据
-             * 但是，考虑到，接口返回事件不会很及时
-             * 所以，动画方法设置为两段执行。
-             * 0s-1s：执行动画本体；
-             * 设置动画本体，为5-10个关键帧动画，不足5个差额的，递增几个数，就是几个关键帧
-             * 2s-5s：执行clearInterval
-             * 当5s结束时，理论上接口返回新的数据，就会重新开启这个loop过程；
-             * 即便接口无法及时返回数据，也只是loop中间的停滞时间增加，感知较小
-             * 
-             * @param {number} newVal 新的值
-             * @param {number} oldVal 旧的值
-             */
-            setLoop (newVal, oldVal) {
-                let diff = newVal - oldVal
-                let eachTime = 0
-                let eachAdd = 0
-                let rest = 0
-                if (diff <= 10) {
-                    eachAdd = 1
-                    eachTime = 1000/diff
-                } else {
-                    eachAdd = parseInt(diff / 10) // 获取每次累加最大整数
-                    rest = diff % 10 // 获取最后一次所需额外增加的数字
-                    eachTime = 100
-                }
-                // console.log('eachTime', eachTime)
-                this.timer = setInterval(() => {
-                    this.restTime += 1
-                    // console.log(this.restTime)
-                    if (diff <= 10 && this.restTime == diff) {
-                        this.fetchNum += eachAdd
-                        this.restTime = 0
-                    } else if (this.restTime == 10 && diff > 10) {
-                        this.fetchNum += (eachAdd + rest)
-                        this.restTime = 0
-                    } else {
-                        this.fetchNum += eachAdd
-                    }
-                    this.transNumber(this.fetchNum)
-                    if (this.fetchNum == newVal) {
-                        // console.log('clean')
-                        clearInterval(this.timer)
-                    }
-                }, eachTime)
+                this.fetchNum = num // 完成动画后更新为newNum
             }
 		}
     }
 </script>
 
 <style lang="less" scoped>
+@fontH: 46px;
+
 .number-bord{
     position: relative;
     &::before{
@@ -184,17 +156,14 @@
                 height: 46px;
                 background: linear-gradient(180deg, #337CFF 0%, rgba(0, 255, 255, 0.11) 24%, rgba(0, 255, 255, 0) 72%, #337CFF 100%);
             }
-            .font{
+            .scroll-out-box{
                 position: absolute;
                 left: -4px;
                 top: 7px;
                 line-height: 46px;
                 width: 100%;
                 height: 100%;
-                text-align: center;
-                color: #ffffff;
-                font-size: 30px;
-                z-index: 3;
+                overflow: hidden;
                 &.comma{
                     &::after{
                         content: ',';
@@ -203,6 +172,51 @@
                         position: absolute;
                         bottom: -2px;
                         right: -4px;
+                    }
+                }
+                .scroll-box{
+                    position: absolute;
+                    top: 0;
+                    width: 30px;
+                    height: auto;
+                    transition: top 1s;
+                    &.number-0{
+                        top: 0;
+                    }
+                    &.number-1{
+                        top: -@fontH;
+                    }
+                    &.number-2{
+                        top: -@fontH * 2;
+                    }
+                    &.number-3{
+                        top: -@fontH * 3;
+                    }
+                    &.number-4{
+                        top: -@fontH * 4;
+                    }
+                    &.number-5{
+                        top: -@fontH * 5;
+                    }
+                    &.number-6{
+                        top: -@fontH * 6;
+                    }
+                    &.number-7{
+                        top: -@fontH * 7;
+                    }
+                    &.number-8{
+                        top: -@fontH * 8;
+                    }
+                    &.number-9{
+                        top: -@fontH * 9;
+                    }
+                    .font{
+                        line-height: 46px;
+                        width: 30px;
+                        height: 46px;
+                        text-align: center;
+                        color: #ffffff;
+                        font-size: 30px;
                     }
                 }
             }
