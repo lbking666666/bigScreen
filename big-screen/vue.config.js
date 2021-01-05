59ce76311cdda53c15c90e631fbd6b9566c11f1c
“lbking666666” <lbking666666@qq.com>
2021-01-05 16:30:32 +0800

    上传控件bug修复

--- big-screen/vue.config.js
+++ big-screen/vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     publicPath:'./',
     devServer: {
+        host:'localhost',
+        port:'8080',
         proxy: {
            '/wep-oms': {
                 //target: 'http://10.124.193.151:8082',
