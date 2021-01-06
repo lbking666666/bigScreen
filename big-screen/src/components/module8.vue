94ba53bee5d21c6886f4c41e28bbbe1838424d87
SilentCoder <1063435563@qq.com>
2021-01-06 17:51:44 +0800

    commit

--- big-screen/src/components/module8.vue
+++ big-screen/src/components/module8.vue
@@ -1,17 +1,24 @@
 <template>
-    <div >
-		
+    <div class="ranking">
+		<div class="title">
+			<CommonTitle :titleText='titleText' />
+		</div>
     </div>
 </template>
 
 <script>
+	import CommonTitle from './commonTitle'
     export default {
 		name: "",
+		components: {
+			CommonTitle
+		},
 		props: {
 			
 		},
 		data(){
 			return{
+				titleText: '1月热销产品/常用功能TOP10'
 			}
 		},
 		mounted() {
@@ -23,5 +30,14 @@
 </script>
 
 <style lang="less" scoped>
+.ranking{
+	box-sizing: border-box;
+	width: 460px;
+	height: 500px;
+	background: url(../assets/yaxin/kuang_zuozhong.png) no-repeat;
+	background-size: 100% 100%;
+	.title{
 
+	}
+}
 </style>
