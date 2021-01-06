93c11e87285ec748a035775c762b4a2408feb49c
SilentCoder <1063435563@qq.com>
2021-01-06 11:58:50 +0800

    commit

--- big-screen/src/components/module5.vue
+++ big-screen/src/components/module5.vue
@@ -1,5 +1,9 @@
 <template>
-    <div >
+    <div class="bar-chart-box">
+		<div class=""></div>
+		<div>
+			<div ref="chart" style="width:100%;height:100%"></div>
+		</div>
 		
     </div>
 </template>
@@ -23,5 +27,11 @@
 </script>
 
 <style lang="less" scoped>
+.bar-chart-box{
+	width: 894px;
+	height: 288px;
+	box-sizing: border-box;
+	padding-top: 44px;
 
+}
 </style>
