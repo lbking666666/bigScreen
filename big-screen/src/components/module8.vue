859eb187c23c4cda7a8f5fc4e2a0f904482b9aea
SilentCoder <1063435563@qq.com>
2021-01-07 10:28:10 +0800

    排行榜

--- big-screen/src/components/module8.vue
+++ big-screen/src/components/module8.vue
@@ -7,6 +7,23 @@
 			<div class="tab-item" :class="tabIndex===0 ? 'active' : ''" @click="tabChange(0)">热销产品</div>
 			<div class="tab-item" :class="tabIndex===1 ? 'active' : ''" @click="tabChange(1)">常用功能</div>
 		</div>
+		<div class="ranking-box">
+			<!-- title在Home中 -->
+			<div class="ranking-header">
+				<div class="index">排名</div>
+				<div class="name">业务名称</div>
+				<div class="order-num">订单量(笔)</div>
+			</div>
+			<div class="ranking-body">
+				<div class="rank-item" v-for="(item, index) in activeList" :key="index">
+					<div class="index" v-if="index >= 3">{{String(index+1)}}</div>
+					<div class="index" v-else><i class="index-icon" :class="`icon-${index}`"></i></div>
+
+					<div class="name">{{item.name}}</div>
+					<div class="order-num">{{item.number}}</div>
+				</div>
+			</div>
+		</div>
     </div>
 </template>
 
@@ -18,15 +35,26 @@
 			CommonTitle
 		},
 		props: {
-			
+			module8Data: {
+				type: Object,
+				default: () => ({})
+			}
 		},
 		data(){
 			return{
 				titleText: '1月热销产品/常用功能TOP10',
 				tabIndex: 0, // 0: 热销产品； 1: 常用功能；
+				activeList: [],
+				listNames: ['hot', 'normal']
 			}
 		},
 		mounted() {
+			let listName = this.listNames[this.tabIndex]
+			this.activeList = this.module8Data[listName]
+		},
+		updated() {
+			let listName = this.listNames[this.tabIndex]
+			this.activeList = this.module8Data[listName]
 		},
 		methods:{
 			tabChange (tab) {
@@ -43,7 +71,7 @@
 	height: 500px;
 	background: url(../assets/yaxin/kuang_zuozhong.png) no-repeat;
 	background-size: 100% 100%;
-	padding-top: 44px;
+	padding-top: 86px;
 	position: relative;
 	.title{
 		position: absolute;
@@ -57,31 +85,179 @@
 		left: 80px;
 		width: 300px;
 		height: 30px;
-		overflow: hidden;
-		border-radius: 15px;
-		border: 1px solid #3A66D0;
 		display: flex;
 		.tab-item{
+			box-sizing: border-box;
 			position: absolute;
 			width: 150px;
-			line-height: 30px;
+			line-height: 28px;
+			height: 30px;
 			font-size: 14px;
 			font-family: PingFangSC-Semibold, PingFang SC;
 			font-weight: 600;
 			color: #C7FCFC;
 			text-align: center;
+			cursor: pointer;
 			&:first-child{
 				top: 0;
 				left: 0;
+				overflow: hidden;
+				border-radius: 15px 0 0 15px;
+				border-top: 1px solid #3A66D0;
+				border-left: 1px solid #3A66D0;
+				border-bottom: 1px solid #3A66D0;
 			}
 			&:last-child{
 				top: 0;
 				right: 0;
+				overflow: hidden;
+				border-radius: 0 15px 15px 0;
+				border-top: 1px solid #3A66D0;
+				border-right: 1px solid #3A66D0;
+				border-bottom: 1px solid #3A66D0;
 			}
 			&.active{
-				// border: 1px solid #3A66D0;
-				box-shadow: 8px 8px 11px 0px rgba(66, 157, 249, 0.5), -8px -8px 11px 0px rgba(63, 176, 236, 0.33);
+				box-shadow: 0 0 11px 8px rgba(66, 157, 249, 0.5) inset;
 			}
+		}
+	}
+	.ranking-box{
+		height: 100%;
+		width: 100%;
+		text-align: left;
+		box-sizing: border-box;
+		color: #fff;
+		font-size: 14px;
+		padding: 0 23px 0 21px;
+		.ranking-header{
+			line-height: 14px;
+			margin-top: 1px;
+			display: flex;
+			.index,
+			.name,
+			.order-num{
+				font-size: 14px;
+				font-family: PingFangSC-Medium, PingFang SC;
+				font-weight: 500;
+				color: #01FFFF;
+				line-height: 14px;
+				text-shadow: 0px 0px 4px rgba(1, 255, 255, 0.6);
+			}
+		}
+		.ranking-body{
+			text-align: center;
+			margin-top: 12px;
+			.rank-item{
+				height: 34px;
+				line-height: 34px;
+				margin-top: 5px;
+				display: flex;
+				position: relative;
+				&:before{
+					content: '';
+					position: absolute;
+					width: 100%;
+					height: 100%;
+					top: 0;
+					left: 0;
+					background: linear-gradient(90deg, #24517F 0%, rgba(0, 184, 252, 0) 100%);
+					opacity: 0.4;
+				}
+				&:first-child{
+					margin-top: 0;
+				}
+				&:nth-child(1),
+				&:nth-child(2),
+				&:nth-child(3){
+					position: relative;
+					height: 30px;
+					line-height: 30px;
+					&:before{
+						content: '';
+						position: absolute;
+						width: 100%;
+						height: 100%;
+						top: 0;
+						left: 0;
+						opacity: 0.4;
+					}
+					&:after{
+						content: '';
+						position: absolute;
+						top: 0;
+						left: 0;
+						width: 3px;
+						height: 30px;
+					}
+				}
+				&:nth-child(1){
+					&:before{
+						background: linear-gradient(90deg, rgba(255, 243, 60, 0.59) 0%, rgba(0, 184, 252, 0) 100%);
+					}
+					&:after{
+						background: rgba(255, 243, 60, 1);
+					}
+				}
+				&:nth-child(2){
+					&:before{
+						background: linear-gradient(90deg, rgba(102, 241, 154, 0.64) 0%, rgba(0, 184, 252, 0) 100%);
+					}
+					&:after{
+						background: rgba(103, 241, 153, 1);
+					}
+				}
+				&:nth-child(3) {
+					&:before {
+						background: linear-gradient(90deg, rgba(0, 252, 241, 0.5) 0%, rgba(0, 184, 252, 0) 100%);
+					}
+					&:after {
+						background: rgba(1, 255, 255, 1);
+					}
+				}
+				.growth-rate{
+					color: #75EF9E;
+				}
+			}
+		}
+		.index{
+			width: 73px;
+			text-align: center;
+			padding-right: 31px;
+			.index-icon{
+				display: inline-block;
+				width: 20px;
+				height: 15px;
+				vertical-align: text-top;
+				&.icon-0{
+					background: url('../assets/yaxin/diyi.png') no-repeat;
+					background-size: contain;
+				}
+				&.icon-1{
+					background: url('../assets/yaxin/dier.png') no-repeat;
+					background-size: contain;
+				}
+				&.icon-2{
+					background: url('../assets/yaxin/disan.png') no-repeat;
+					background-size: contain;
+				}
+			}
+		}
+		.name{
+			width: 220px;
+			text-align: left;
+			font-size: 14px;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #FFFFFF;
+		}
+		.order-num{
+			width: 133px;
+			text-align: center;
+			padding-right: 57px;
+			font-size: 14px;
+			font-family: PingFangSC-Medium, PingFang SC;
+			font-weight: 500;
+			color: #C7FCFC;
 		}
 	}
 }
