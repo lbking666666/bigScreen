ae98b84dbec775a53ff3cf42a06880e8954e664c
unknown <2207252182@qq.com>
2021-01-14 22:32:33 +0800

    commit

--- big-screen/src/components/module8.vue
+++ big-screen/src/components/module8.vue
@@ -1,36 +1,72 @@
 <template>
     <div class="rose-charts">
         <div class="title-box">
-            <commonTitle :titleText="titleText"></commonTitle>
+            <commonTitle :titleText="titleText" />
         </div>
     	
     	<div class="bar-box">
-	        <div class="bar-item">热销产品</div>
-            <div class="bar-item">常用功能</div>
-	    </div>
+            <tabs :barNames="barNames" @checkItem="changeBar" />
+        </div>
 
         <div class="top-boxs">
-            <div class="top-2-box"></div>
+            <div class="top-2-box">
+                <div class="title">TOP2</div>
+                <div class="bg">
+                    <p class="value">{{moduleData[activeIndex].second.val}}</p>
+                    <p class="name">{{moduleData[activeIndex].second.name}}</p>
+                </div>
+            </div>
+            <div class="top-1-box">
+                <div class="title">TOP1</div>
+                <div class="bg">
+                    <p class="value">{{moduleData[activeIndex].first.val}}</p>
+                    <p class="name">{{moduleData[activeIndex].first.name}}</p>
+                </div>
+            </div>
+            <div class="top-3-box">
+                <div class="title">TOP3</div>
+                <div class="bg">
+                    <p class="value">{{moduleData[activeIndex].third.val}}</p>
+                    <p class="name">{{moduleData[activeIndex].third.name}}</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
+
 <script>
-let chart = null
-import * as echarts from "echarts";
-import commonTitle from "./commonTitle";
+import commonTitle from "./commonTitle.vue";
+import tabs from '@/components/tabs.vue';
 import {formatterNumber} from '@/utils/filterNum'
 export default {
     name: "roseCharts",
+    components: {
+        commonTitle,
+        tabs
+    },
     props: {
         moduleData:{
             type:Array,
             default:[]
         }
     },
-    components:{commonTitle},
     data() {
         return {
-        	titleText:'热销产品/常用功能TOP3'
+            titleText:'热销产品/常用功能TOP3',
+            barNames: ['热销产品', '常用功能'],
+            moduleData: [
+                {
+                    first: {name: '普通付费关系', val: 86687},
+                    second: {name: '换补卡', val: 6687},
+                    third: {name: '换网', val: 687}
+                },
+                {
+                    first: {name: '', val: 86687},
+                    second: {name: '', val: 86687},
+                    third: {name: '', val: 86687}
+                }
+            ],
+            activeIndex: 0
         }
     },
     mounted() {
@@ -38,9 +74,14 @@ export default {
     updated() {
     },
     methods: {
+        changeBar (num) {
+            console.log('num', num)
+            this.activeIndex = num
+        }
     }
 }
 </script>
+
 <style lang="less" scoped>
 .rose-charts{
     box-sizing: border-box;
@@ -48,8 +89,67 @@ export default {
 	height:280px;
 	background:url(../assets/yaxin/kuang_rexiaochangyong.png) no-repeat;
     background-size: cover;
+    position: relative;
     .title-box{
         margin-top: 5px;
+    }
+    .bar-box{
+        position: absolute;
+        top: 52px;
+        left: 80px;
+        width: 300px;
+        height: 32px;
+    }
+
+    .top-boxs{
+        .top-1-box{
+            position: absolute;
+            top: 103px;
+            left: 147px;
+            .title{
+                font-size: 13px;
+                color: #ffe578;
+                padding-bottom: 8px;
+            }
+            .bg{
+                height: 113px;
+                width: 168px;
+                background:url(../assets/yaxin/top1.png) no-repeat;
+                background-size: cover;
+            }
+        }
+        .top-2-box{
+            position: absolute;
+            top: 118px;
+            left: 14px;
+            .title{
+                font-size: 13px;
+                color: #40f5f8;
+                padding-bottom: 6px;
+            }
+            .bg{
+                height: 124px;
+                width: 124px;
+                background:url(../assets/yaxin/top_2.png) no-repeat;
+                background-size: cover;
+            }
+        }
+        .top-3-box{
+            position: absolute;
+            top: 118px;
+            right: 14px;
+            .title{
+                font-size: 13px;
+                color: #40f5f8;
+                padding-bottom: 6px;
+            }
+            .bg{
+                height: 124px;
+                width: 124px;
+                background:url(../assets/yaxin/top_2.png) no-repeat;
+                background-size: cover;
+            }
+        }
     }
 }
 </style>
