e205424a71088e5f657944eb1ef829cdf7882092
unknown <2207252182@qq.com>
2021-01-15 01:14:59 +0800

    commit
    添加proxy

--- big-screen/vue.config.js
+++ big-screen/vue.config.js
@@ -9,6 +9,24 @@ module.exports = {
                 ws: false,
                 pathRewrite: { '^/ITTest': '/ITTest' }
             },
+            '/BaseUrlA': {
+                target: 'http://10.161.67.41:9002',
+                changeOrigin: true,
+                ws: false,
+                pathRewrite: { '^/BaseUrlA': '/BaseUrlA' }
+            },
+            '/BaseUrlB': {
+                target: 'http://10.161.12.1:9099',
+                changeOrigin: true,
+                ws: false,
+                pathRewrite: { '^/BaseUrlB': '/BaseUrlB' }
+            },
+            '/BaseUrlC': {
+                target: 'http://10.161.12.101:8622',
+                changeOrigin: true,
+                ws: false,
+                pathRewrite: { '^/BaseUrlC': '/BaseUrlC' }
+            },
         }
     }
 }
